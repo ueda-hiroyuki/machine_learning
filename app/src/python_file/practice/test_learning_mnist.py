239212799d5bf83e03a2bb912bb2568cdabcd40c
ueda <uehiro0057@gmail.com>
2020-04-14 03:23:24 +0000

    fix

--- app/src/python_file/practice/test_learning_mnist.py
+++ app/src/python_file/practice/test_learning_mnist.py
@@ -6,6 +6,20 @@ import typing as t
 from sample_data.deep_learning_documents.dataset import mnist as mn
 
 
+class simpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2,3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax_func(z)
+        loss = cross_entropy_error(y, t)
+        return loss
+
+
 def load_data():
     (x_train, t_train), (x_test, t_test) = mn.load_mnist(normalize=True, one_hot_label=True)
     return x_train, t_train
@@ -23,7 +37,6 @@ def cross_entropy_error(y: t.Sequence, t: t.Sequence) -> t.Sequence:
         t = t.reshape(1, t.size)
     batch_size = y.shape[0]
     e = - np.sum(t * np.log(y + delta)) / batch_size # +deltaはlog0(-inf)とならないような対策
-    print(e)
     return e
 
 
@@ -38,22 +51,42 @@ def numerical_grad(f, x):
     h = 1e-4
     grad = np.zeros_like(x) # xと同じ形状の配列を生成
     for idx in range(x.size):
-        fake_x = x 
-        val = fake_x[idx]
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
         
-        fake_x[idx] = val + h
-        f1 = f(fake_x) # f(x+h)
-
-        fake_x[idx] = val - h
-        f2 = f(fake_x) # f(x-h)
-
-        grad[idx] = (f1-f2) / (2*h)
+        x[idx] = tmp_val - h 
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val  # 値を元に戻す
     return grad
+
+# 勾配降下法
+def gradient_descent(func, init_x, lr=0.01, step_num=100): # 勾配降下法
+    x = init_x
+    for n in range(step_num):
+        grad = numerical_grad(func, x) # 勾配の算出
+        print(grad)
+        x = x - lr * grad
+    return x
 
 
 def func_2(x):
     f = np.sum(x**2)
     return f
+
+
+def sigmoid_func(x):
+    y = 1 / (1 + np.exp(-x))
+    return y 
+
+
+def softmax_func(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c) # オーバーフロー対策
+    sum_exp_a = np.sum(exp_a)
+    return exp_a / sum_exp_a
 
 
 def exec():
@@ -67,9 +100,12 @@ def exec():
     batch_t_train = t_train[batch_mask]
 
 def main():
-    diff = numerical_grad(func_2, np.array([3.0,4.0]))
-    print(diff)
-
+    simple_net = simpleNet()
+    x = np.array([0.6, 0.9])
+    p = simple_net.predict(x)
+    t = np.array([0,0,1])
+    loss = simple_net.loss(x,t)
+    print(loss)
 
 if __name__ == "__main__":
     main()
