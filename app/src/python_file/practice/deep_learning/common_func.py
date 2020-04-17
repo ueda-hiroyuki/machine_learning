7ec4700940c829cbac4fbcf5ee371c80e6acc680
ueda <uehiro0057@gmail.com>
2020-04-17 12:39:06 +0000

    add common_func & common_layers

--- app/src/python_file/practice/deep_learning/common_func.py
+++ app/src/python_file/practice/deep_learning/common_func.py
@@ -7,32 +7,50 @@ import typing as t
 
 
 class CommonFunctions:
-    def __init__(self):
-        pass
+    def identity_function(self, x):
+        return x
 
-    def softmax_func(self, a):
-        c = np.max(a)
-        exp_a = np.exp(a - c) # オーバーフロー対策
-        sum_exp_a = np.sum(exp_a)
-        return exp_a / sum_exp_a
-    
-    def sigmoid_func(self, x):
-        y = 1 / (1 + np.exp(-x))
-        return y 
 
-    def mean_squared_error(self, y: t.Sequence, t: t.Sequence) -> t.Sequence:
-        e = 1/2 * (np.sum((y-k)**2))
-        return e
+    def step_function(self, x):
+        return np.array(x > 0, dtype=np.int)
+
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))    
+
+
+    def sigmoid_grad(self, x):
+        return (1.0 - sigmoid(x)) * sigmoid(x)
+        
+
+    def relu(self, x):
+        return np.maximum(0, x)
+
+
+    def relu_grad(self, x):
+        grad = np.zeros_like(x)
+        grad[x>=0] = 1
+        return grad
+        
+
+    def softmax(self, x):
+        x = x - np.max(x, axis=-1, keepdims=True)   # オーバーフロー対策
+        return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
+
+
+    def sum_squared_error(self, y, t):
+        return 0.5 * np.sum((y-t)**2)
+
 
     def cross_entropy_error(self, y, t):
         if y.ndim == 1:
             t = t.reshape(1, t.size)
             y = y.reshape(1, y.size)
             
-        # 教師データ(t)がone-hot-vectorの場合、正解ラベルのインデックスに変換される ⇒ ex) [0,0,1,0,0,0] = [2]
+        # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
         if t.size == y.size:
-            t = np.argmax(t, axis=1)
+            t = t.argmax(axis=1)
                 
         batch_size = y.shape[0]
         return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
- 
+
