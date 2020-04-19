924fc6d308ba89914039d1e53d59a1b37908f277
ueda <uehiro0057@gmail.com>
2020-04-19 14:23:30 +0000

    fix simple_conv_net

--- app/src/python_file/practice/deep_learning/practice_cnn.py
+++ app/src/python_file/practice/deep_learning/practice_cnn.py
@@ -1,11 +1,12 @@
 import numpy as np
 from sample_data.deep_learning_documents.common.util import *
 from python_file.practice.deep_learning.common_layers import *
+from python_file.practice.deep_learning.simple_conv_net import *
 
 def main():
     img = np.random.randint(0,9,(2,3,4,4))
-    pool = Pooling(pool_h=2, pool_w=2)
-    out = pool.forward(img)
+    network = SimpleConvNet()
+
 
 
 if __name__ == "__main__":
--- app/src/python_file/practice/deep_learning/simple_conv_net.py
+++ app/src/python_file/practice/deep_learning/simple_conv_net.py
@@ -1,5 +1,44 @@
+import joblib
+import logging
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import typing as t
+from collections import OrderedDict
 from python_file.practice.deep_learning.common_func import *
 from python_file.practice.deep_learning.common_layers import *
 
+logging.basicConfig(level=logging.INFO)
 
+# 入力層:1、隠れ層:1、出力層:1の2層ニューラルネットワーク
+class SimpleConvNet:
+    def __init__(
+        self,
+        input_dim=(1,28,28), # 入力画像1枚の次元(チャンネル数:1、幅:28、高さ:28)
+        conv_param={
+            'filter_num': 30, # フィルターの枚数
+            'filter_size': 5, # フィルターの幅
+            'padding': 0,
+            'stride': 1
+        },
+        hidden_size=100,
+        output_size=10,
+        weight_init_std=0.01 # 重みの標準偏差
+    ):
+        filter_num = conv_param['filter_num']
+        filter_size = conv_param['filter_size']
+        filter_padding = conv_param['padding']
+        filter_stride = conv_param['stride']
+        input_size = input_dim[1]
+        conv_output_size = (input_size + 2*filter_padding - filter_size) / filter_stride + 1
+        pool_output_size = int((input_size - filter_size) / filter_stride + 1)
+
+        print(filter_num, filter_size, input_size, conv_output_size, pool_output_size)
+
+
+
+        # # 重みの初期化
+        # self.params = {}
+        # self.params["W1"] = weight_init_std * np.random.randn(filter_num, input_dim[0], filter_size, filter_size)
+        # self.params["b1"] = np.zeros(filter_num)
+        # self.params["W2"] = weight_init_std * np.random.randn(filter_num, )
