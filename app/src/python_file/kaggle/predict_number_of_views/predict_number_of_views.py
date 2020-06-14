68db8f938c8e500e2914a65342f7e85f813721f1
ueda hiroyuki <uehiro0057@gmail.com>
2020-06-14 06:50:04 +0000

    add predict_number_of_views

--- app/src/python_file/kaggle/predict_number_of_views/predict_number_of_views.py
+++ app/src/python_file/kaggle/predict_number_of_views/predict_number_of_views.py
@@ -16,6 +16,22 @@ from sklearn.preprocessing import StandardScaler, MinMaxScaler, LabelEncoder, On
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.feature_selection import RFE
 
+DATA_DIR = "src/sample_data/Kaggle/predict_number_of_views"
+TRAIN_DATA = f"{DATA_DIR}/train_data.csv"
+TEST_DATA = f"{DATA_DIR}/test_data.csv"
+
+
+def main():
+    raw_train = pd.read_csv(TRAIN_DATA)
+    raw_test = pd.read_csv(TEST_DATA)
+
+    print(raw_train, raw_train.columns, raw_train.info())
+
+
+
+if __name__ == "__main__":
+    main()
+
 
 
 
