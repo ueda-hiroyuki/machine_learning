5bfa6200986caafecdf630b9bbd86f2c173af04b
ueda hiroyuki <uehiro0057@gmail.com>
2020-09-02 13:06:20 +0000

    add image_predictor

--- app/src/python_file/practice/pytorch/section-1/load_vgg.py
+++ app/src/python_file/practice/pytorch/section-1/load_vgg.py
@@ -1,5 +1,8 @@
 import numpy as np
 import json
+import os
+import urllib.request
+import zipfile
 from PIL import Image
 import matplotlib.pyplot as plt
 import torch
@@ -33,7 +36,37 @@ class BasePreprocessor:
         return self.base_transform(img)
 
 
+class ILSVRCPredictor:
+    """
+    ILSVRCデータに対するモデルの出力からラベルを求めるクラス
+    ・input
+        class_index: 辞書型で定義されたラベル
+    """
+
+    def __init__(self, class_index):
+        self.class_index = class_index
+
+    def predict_label(self, out):
+        """
+        予測確率が最大のラベル名を出力する
+        ・input
+            out: vgg16からの出力(torch.Size([1,1000]))
+
+        """
+        max_id = np.argmax(
+            out.detach().numpy()
+        )  # Tensor型のデータをNumpy型に変換する(detach：netからの切り離し)
+        predicted_label = self.class_index[str(max_id)][
+            1
+        ]  # max_idに対応したラベルをclass_indexから取得する
+
+        return predicted_label
+
+
 def main():
+    ILSVRC_class_index = json.load(open(f"{DATA_DIR}/imagenet_class_index.json", "r"))
+    predictor = ILSVRCPredictor(ILSVRC_class_index)
+
     use_pre_trained = True  # 学習済みのパラメータを使用する
     net = models.vgg16(pretrained=use_pre_trained)  # 学習済みのvgg16モデルのインスタンスを作成する。
     net.eval()  # 推論モードに変更
@@ -47,11 +80,18 @@ def main():
 
     transform = BasePreprocessor(resize, mean, std)
     img_transformed = transform(img)  # call関数実行 ⇒ return (3,224,224)
-    img_transformed = img_transformed.numpy().transpose(
-        (1, 2, 0)
-    )  # transpose to (224,224,3)
-    img_transformed = np.clip(img_transformed, 0, 1)  # 数値を指定の範囲に規格化する
-    print(img_transformed)
+    # img_transformed = img_transformed.transpose((1, 2, 0))  # transpose to (224,224,3)
+    # img_transformed = np.clip(img_transformed, 0, 1)  # 数値を指定の範囲に規格化する
+    inputs = img_transformed.unsqueeze_(
+        0
+    )  # reshape to torch.Size[(1, 3, 224, 224)] ← この形にしないとモデルは検知できない
+
+    # モデルに入力
+    out = net(inputs)
+
+    predicted_label = predictor.predict_label(out)
+
+    print(predicted_label)
 
 
 if __name__ == "__main__":
