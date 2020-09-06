9ba9b2cb04a2dac41a63af5ae74bc5f09b001660
ueda hiroyuki <uehiro0057@gmail.com>
2020-09-06 09:59:07 +0000

    fix

--- app/src/python_file/kaggle/predict_winner/predict_winner2.py
+++ app/src/python_file/kaggle/predict_winner/predict_winner2.py
@@ -13,26 +13,34 @@ from sklearn.metrics import accuracy_score
 DATA_DIR = "src/sample_data/Kaggle/predict_winner"
 TRAIN_PATH = f"{DATA_DIR}/train_data.csv"
 TEST_PATH = f"{DATA_DIR}/test_data.csv"
-REMOVAL_COLS_FOR_REGULAR = [
-    "lobby",
-    "game-ver",
-    "A1-rank",
-    "A2-rank",
-    "A3-rank",
-    "A4-rank",
-    "B1-rank",
-    "B2-rank",
-    "B3-rank",
-    "B4-rank",
-]
-REMOVAL_COLS_FOR_GACHI = ["lobby", "game-ver"]
+REMOVAL_COLS = ["lobby", "game-ver"]
 
 
-def preprocess(df):
-    categorical_columns = [x for x in df.columns if df[x].dtype == "object"]
+def preprocess(train_path, test_path):
+    train_raw_data = pd.read_csv(train_path)
+    test_raw_data = pd.read_csv(test_path)
+
+    test_raw_data["y"] = 0
+    train_raw_data["usage"] = 0  # for train
+    test_raw_data["usage"] = 1  # for test
+
+    raw_data = pd.concat([train_raw_data, test_raw_data], axis=0).reset_index(drop=True)
+
+    # filterd_raw_data = raw_data.drop(
+    #     [x for x in raw_data.columns if "rank" in x],
+    #     axis=1,  # 'rank'を含んだ列は何nanが多いため、とりあえず除去
+    # )
+    filterd_raw_data = raw_data
+    filterd_raw_data = filterd_raw_data.fillna(filterd_raw_data.mode().iloc[0]).drop(
+        REMOVAL_COLS, axis=1
+    )
+
+    categorical_columns = [
+        x for x in filterd_raw_data.columns if filterd_raw_data[x].dtype == "object"
+    ]
 
     ce_oe = ce.OrdinalEncoder(cols=categorical_columns, handle_unknown="impute")
-    encorded_data = ce_oe.fit_transform(df)
+    encorded_data = ce_oe.fit_transform(filterd_raw_data)
 
     train = (
         encorded_data[encorded_data["usage"] == 0]
@@ -85,10 +93,10 @@ def train(train_x, train_y, kfold, best_params=None):
     params = {
         "objective": "binary",
         "boosting_type": "gbdt",
-        "metric": None,
-        "num_leaves": 100,
-        "min_data_in_leaf": 1000,
-        "learning_rate": 0.01,
+        "metric": {"binary_logloss"},
+        "num_leaves": 50,
+        "min_data_in_leaf": 100,
+        "learning_rate": 0.1,
         "feature_fraction": 0.7,
     }
     models = []
@@ -145,36 +153,9 @@ def predict(model, test_x, threshold):
 
 
 def run_all():
-    train_raw_data = pd.read_csv(TRAIN_PATH)
-    test_raw_data = pd.read_csv(TEST_PATH)
-
-    test_raw_data["y"] = 0
-    train_raw_data["usage"] = 0  # for train
-    test_raw_data["usage"] = 1  # for test
-
-    raw_data = pd.concat([train_raw_data, test_raw_data], axis=0).reset_index(drop=True)
-
-    # lobby-mode列が'regular'のデータ
-    raw_regular_data = raw_data[raw_data["lobby-mode"] == "regular"].drop(
-        REMOVAL_COLS_FOR_REGULAR, axis=1
-    )
-    raw_regular_data = raw_regular_data.fillna(raw_regular_data.mode().iloc[0])
-
-    # lobby-mode列が'gachi'のデータ
-    raw_gachi_data = raw_data[raw_data["lobby-mode"] == "gachi"].drop(
-        REMOVAL_COLS_FOR_GACHI, axis=1
-    )
-    raw_gachi_data = raw_gachi_data.fillna(raw_gachi_data.mode().iloc[0])
-
-    # train用とtest用のデータの前処理(regular)
-    train_x_for_regular, train_y_for_regular, test_x_for_regular = preprocess(
-        raw_regular_data
-    )
-    ids_for_regular = test_x_for_regular.loc[:, "id"]
-
-    # train用とtest用のデータの前処理(gachi)
-    train_x_for_gachi, train_y_for_gachi, test_x_for_gachi = preprocess(raw_gachi_data)
-    ids_for_gachi = test_x_for_gachi.loc[:, "id"]
+    # train用とtest用のデータの前処理
+    train_x, train_y, test_x = preprocess(TRAIN_PATH, TEST_PATH)
+    ids = test_x.loc[:, "id"]
 
     # # 学習用のハイパラをチューニング
     # best_params = get_best_params(train_x, train_y)
@@ -183,41 +164,21 @@ def run_all():
     # 学習
     n_splits = 5
     kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=0)
-
-    models_for_regular = train(train_x_for_regular, train_y_for_regular, kfold)
-    models_for_gachi = train(train_x_for_gachi, train_y_for_gachi, kfold)
+    models = train(train_x, train_y, kfold)
 
     # 評価
     threshold = 0.5
-    y_preds_for_regular = []
-    y_preds_for_gachi = []
-    for i, (model_for_regular, model_for_gachi) in enumerate(
-        zip(models_for_regular, models_for_gachi)
-    ):
-        y_pred_for_regular = predict(model_for_regular, test_x_for_regular, threshold)
-        y_pred_for_gachi = predict(model_for_gachi, test_x_for_gachi, threshold)
-        y_preds_for_regular.append(y_pred_for_regular)
-        y_preds_for_gachi.append(y_pred_for_gachi)
+    y_preds = []
+    for i, model in enumerate(models):
+        y_pred = predict(model, test_x, threshold)
+        y_preds.append(y_pred)
 
     # 提出用ファイル成型
-    winner_pred_for_regular = (
-        pd.concat(y_preds_for_regular, axis=1).mode(axis=1).rename(columns={0: "y"})
-    )
-    winner_pred_for_gachi = (
-        pd.concat(y_preds_for_gachi, axis=1).mode(axis=1).rename(columns={0: "y"})
-    )
+    winner_pred = pd.concat(y_preds, axis=1).mode(axis=1).rename(columns={0: "y"})
+    submission = pd.concat([ids, winner_pred], axis=1)
 
-    submission_for_regular = pd.concat(
-        [ids_for_regular, winner_pred_for_regular], axis=1
-    )
-    submission_for_gachi = pd.concat([ids_for_gachi, winner_pred_for_gachi], axis=1)
-
-    submission = pd.concat(
-        [submission_for_regular, submission_for_gachi], axis=0
-    ).sort_values("id")
-
-    print(submission.head(50), submission)
-    submission.to_csv(f"{DATA_DIR}/submission4.csv", index=False)
+    print(submission)
+    submission.to_csv(f"{DATA_DIR}/submission5.csv", index=False)
 
 
 if __name__ == "__main__":
