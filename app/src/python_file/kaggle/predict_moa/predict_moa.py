9b1c277d8ccaf1d44dfb209e3c4e72d7daee055f
ueda hiroyuki <uehiro0057@gmail.com>
2020-09-20 02:35:37 +0000

    add predict_moa

---
+++ app/src/python_file/kaggle/predict_moa/predict_moa.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+
+DATA_DIR = "src/sample_data/Kaggle/predict_moa"
+
+
+def main():
+    train_raw = pd.read_csv(f"{DATA_DIR}/train_features.csv")
+    test_raw = pd.read_csv(f"{DATA_DIR}/test_features.csv")
+    train_target_nonscored_raw = pd.read_csv(f"{DATA_DIR}/train_targets_nonscored.csv")
+    train_target_scored_raw = pd.read_csv(f"{DATA_DIR}/train_targets_scored.csv")
+
+    print(train_raw, test_raw, train_target_nonscored_raw, train_target_scored_raw)
+
+
+if __name__ == "__main__":
+    main()
