4bad438c2ca55c76ca1216deb92081ee353499c8
ueda hiroyuki <uehiro0057@gmail.com>
2020-10-23 14:34:24 +0000

    fix

--- app/src/python_file/kaggle/predict_winner/feature_engineering.py
+++ app/src/python_file/kaggle/predict_winner/feature_engineering.py
@@ -3,6 +3,8 @@ import random
 import pandas as pd
 import numpy as np
 import lightgbm as lgb
+import matplotlib.pyplot as plt
+import category_encoders as ce
 from sklearn.preprocessing import (
     LabelEncoder,
     LabelBinarizer,
@@ -41,5 +43,83 @@ def encoding_method():
     print(df_trans)
 
 
+def null_importance():
+    def display_distributions(actual_imp_df, null_imp_df, feature, num):
+        # ある特徴量に対する重要度を取得
+        actual_imp = actual_imp_df.query(f"feature == '{feature}'")["importance"].mean()
+        null_imp = null_imp_df.query(f"feature == '{feature}'")["importance"]
+
+        # 可視化
+        fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+        a = ax.hist(null_imp, label="Null importances")
+        ax.vlines(
+            x=actual_imp,
+            ymin=0,
+            ymax=np.max(a[0]),
+            color="r",
+            linewidth=10,
+            label="Real Target",
+        )
+        ax.legend(loc="upper right")
+        ax.set_title(f"Importance of {feature.upper()}", fontweight="bold")
+        plt.xlabel(f"Null Importance Distribution for {feature.upper()}")
+        plt.ylabel("Importance")
+        plt.savefig(f"{DATA_DIR}/{num}th_importance.png")
+
+    def train(X, y):
+        clf = lgb.LGBMClassifier()
+        clf.fit(X, y)
+        # 特徴量の重要度を含むデータフレームを作成
+        imp_df = pd.DataFrame()
+        imp_df["feature"] = X.columns
+        imp_df["importance"] = clf.feature_importances_
+        return imp_df.sort_values("importance", ascending=False)
+
+    data = raw_data.fillna(raw_data.mode().iloc[0])
+    categorical_columns = [x for x in data.columns if data[x].dtype == "object"]
+    ce_oe = ce.OrdinalEncoder(cols=categorical_columns, handle_unknown="impute")
+    encorded_data = ce_oe.fit_transform(data)
+    train_data = (
+        encorded_data[encorded_data["usage"] == 0]
+        .drop(["usage"], axis=1)
+        .reset_index(drop=True)
+    )
+    test_data = (
+        encorded_data[encorded_data["usage"] == 1]
+        .drop(["usage"], axis=1)
+        .reset_index(drop=True)
+    )
+
+    train_y = train_data.loc[:, "y"]
+    train_x = train_data.drop(["y"], axis=1)
+    test_x = test_data.drop(["y"], axis=1)
+
+    # 特徴量の重要度を含むデータフレームを作成(本来のimportance)
+    actual_imp_df = train(train_x, train_y)
+
+    # null importanceの作成
+    trials = 100
+    null_imp_df = pd.DataFrame()
+    for i in range(trials):
+        # 目的変数をシャッフルする
+        y_permuted = np.random.permutation(train_y)
+        imp_df = train(train_x, y_permuted)
+        imp_df["run"] = i + 1
+        null_imp_df = pd.concat([null_imp_df, imp_df])
+
+    # 閾値を設定
+    THRESHOLD = 90
+    # 閾値を超える特徴量を取得
+    imp_features = []
+    for num, feature in enumerate(actual_imp_df["feature"]):
+        display_distributions(actual_imp_df, null_imp_df, feature, num)
+        actual_value = actual_imp_df.query(f"feature=='{feature}'")["importance"].values
+        null_value = null_imp_df.query(f"feature=='{feature}'")["importance"].values
+        percentage = (null_value < actual_value).sum() / null_value.size * 100
+        if percentage >= THRESHOLD:
+            imp_features.append(feature)
+    print(imp_features)
+
+
 if __name__ == "__main__":
-    encoding_method()
+    null_importance()
