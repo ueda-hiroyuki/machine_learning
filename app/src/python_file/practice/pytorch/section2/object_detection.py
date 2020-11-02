62dfe370d668e05fb694fc710cb380af467ccb8d
ueda hiroyuki <uehiro0057@gmail.com>
2020-11-02 13:40:22 +0000

    fix object_detection

--- app/src/python_file/practice/pytorch/section2/object_detection.py
+++ app/src/python_file/practice/pytorch/section2/object_detection.py
@@ -1,12 +1,13 @@
-import os.path as osp
 import torch
 import cv2
 import torchvision
+import os.path as osp
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import xml.etree.ElementTree as ET
 import matplotlib.pyplot as plt
+from itertools import product
 from PIL import Image
 from torchvision import models, transforms
 from torch.utils.data import Dataset, DataLoader
@@ -115,6 +116,74 @@ class VOCDataset(Dataset):
         # アノテーションのpixel情報とラベルをくっつける
         gt = np.hstack((boxes, np.expand_dims(labels, axis=1)))
         return img, gt, height, width
+
+
+class L2Norm(nn.Module):
+    """
+    カスタムレイヤー
+    ・お約束
+        ⇒ nn.Modileを継承する
+        ⇒ レイヤーのparameterはnn.Prameterで設定する。
+        ⇒ 順伝播はforwardメソッドを定義する。
+    """
+
+    def __init__(self, in_channels=512, scale=20):
+        super(L2Norm, self).__init__()  # 親クラスのコンストラクタ実行
+        self.weight = nn.Parameter(torch.Tensor(in_channels))  # nn.Parameterの引数はTensor型
+        self.scale = scale  # 係数weightの初期値として設定する値
+        self.reset_parameters()  # parameterの初期化
+        self.eps = 1e-10
+
+    def reset_parameters(self):
+        # 結合パラメータの値を大きさscaleの値にする初期化を実行
+        init.constant_(self.weight, self.scale)  # weightの値がすべてscale(=20)になる
+
+    def forward(self, x):
+        # norm: チャネル方向の2乗和の平方根
+        norm = x.pow(2).sum(dim=1, keepdim=True).sqrt() + self.eps
+        x = torch.div(x, norm)  # normで割ることで正規化
+
+        # self.weightはtensor([512])なので、tensor([batch_num, 512, 特徴量マップHeight, 特徴量マップwidth])に変換
+        weights = self.weight.unsqueeze(0).unsqueeze(2).unsqueeze(3).expand_as(x)
+        out = weights * x  #  重み * 正規化された入力値
+        return out
+
+
+class DefaultBox:
+    def __init__(self, cfg):
+        super(DefaultBox, self).__init__()
+
+        # 初期設定
+        self.image_size = cfg["input_size"]  # 画像のサイズ(=300)
+        self.feature_maps = cfg[
+            "feature_maps"
+        ]  # 各sourceの特徴量mapのサイズ(38*38, 19*19, 10*10, 5*5, 3*3, 1*1)
+        self.num_priors = len(cfg["feature_maps"]) # sourceの個数(=6)
+        self.steps = cfg["steps"] # DBoxのピクセルサイズ
+        self.min_sizes = cfg["min_sizes"] # 小さい正方形Dboxのサイズ
+        self.max_sizes = cfg["max_sizes"] # 大きい正方形Dboxのサイズ
+        self.aspect_ratio = cfg["aspect_ratio"] # 長方形Dboxのアスペクト比
+
+    def generate_dbox_list(self,):
+        """
+        Dboxのリストを作成する関数
+        """
+        mean = []
+        for n, f in enumerate(self.feature_maps): # featire_maps: (38,19,10,5,3,1)
+            for i, j in product(range(f), repeat=2):
+                # 特徴量マッピングの画像サイズ
+                f_k = self.image_size / self.steps[n] # self.steps[n]: 8,16,32,64,
+                
+                # dboxの中心座標x,y (但し、0~1で規格化している)
+                # 1 pixel毎にbboxを作成するため、+0.5すると1*1の中心部分に相当する
+                # f_k(特徴量マップのサイズ)で割ることで中心座標を0~1に規格化している
+                cx = (j + 0.5) / f_k # dboxの中心x座標
+                cy = (i + 0.5) / f_k # dboxの中心y座標
+
+                # アスペクト比1の小さいdbox(cx, cy, width, height)
+                
+
+
 
 
 def make_data_path_list(root_dir):
