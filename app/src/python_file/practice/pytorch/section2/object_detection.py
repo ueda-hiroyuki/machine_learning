3f451e3a9d410700671febde56f555e56913fbf6
ueda hiroyuki <uehiro0057@gmail.com>
2020-11-03 04:36:41 +0000

    fix object_detection

--- app/src/python_file/practice/pytorch/section2/object_detection.py
+++ app/src/python_file/practice/pytorch/section2/object_detection.py
@@ -40,6 +40,17 @@ voc2012_classes = [
     "tvmonitor",
 ]
 
+cfg_ssd = {
+    "num_classes": 21,  # 20クラス+背景(1クラス)
+    "input_size": 300,  # 画像の入力サイズ
+    "bbox_aspect_num": [4, 6, 6, 6, 4, 4],  # 出力するDboxのアスペクト比の種類
+    "feature_maps": [38, 19, 10, 5, 3, 1],  # 特徴量マッピング(各source)のサイズ
+    "steps": [8, 16, 32, 64, 100, 300],  # dboxの大きさ
+    "min_sizes": [30, 60, 111, 162, 213, 264],  # dboxの大きさ
+    "max_sizes": [60, 111, 162, 213, 264, 315],  # dboxの大きさ
+    "aspect_ratio": [[2], [2, 3], [2, 3], [2, 3], [2], [2]],
+}
+
 
 class DataTransform:
     """
@@ -136,11 +147,11 @@ class L2Norm(nn.Module):
 
     def reset_parameters(self):
         # 結合パラメータの値を大きさscaleの値にする初期化を実行
-        init.constant_(self.weight, self.scale)  # weightの値がすべてscale(=20)になる
+        nn.init.constant_(self.weight, self.scale)  # weightの値がすべてscale(=20)になる
 
     def forward(self, x):
         # norm: チャネル方向の2乗和の平方根
-        norm = x.pow(2).sum(dim=1, keepdim=True).sqrt() + self.eps
+        norm = x.pow(2).sum(dim=1, keepdim=True).np.sqrt() + self.eps
         x = torch.div(x, norm)  # normで割ることで正規化
 
         # self.weightはtensor([512])なので、tensor([batch_num, 512, 特徴量マップHeight, 特徴量マップwidth])に変換
@@ -158,32 +169,73 @@ class DefaultBox:
         self.feature_maps = cfg[
             "feature_maps"
         ]  # 各sourceの特徴量mapのサイズ(38*38, 19*19, 10*10, 5*5, 3*3, 1*1)
-        self.num_priors = len(cfg["feature_maps"]) # sourceの個数(=6)
-        self.steps = cfg["steps"] # DBoxのピクセルサイズ
-        self.min_sizes = cfg["min_sizes"] # 小さい正方形Dboxのサイズ
-        self.max_sizes = cfg["max_sizes"] # 大きい正方形Dboxのサイズ
-        self.aspect_ratio = cfg["aspect_ratio"] # 長方形Dboxのアスペクト比
+        self.num_priors = len(cfg["feature_maps"])  # sourceの個数(=6)
+        self.steps = cfg["steps"]  # DBoxのピクセルサイズ
+        self.min_sizes = cfg["min_sizes"]  # 小さい正方形Dboxのサイズ
+        self.max_sizes = cfg["max_sizes"]  # 大きい正方形Dboxのサイズ
+        self.aspect_ratio = cfg["aspect_ratio"]  # 長方形Dboxのアスペクト比
 
-    def generate_dbox_list(self,):
+    def generate_dbox_list(
+        self,
+    ):
         """
         Dboxのリストを作成する関数
         """
         mean = []
-        for n, f in enumerate(self.feature_maps): # featire_maps: (38,19,10,5,3,1)
+        for n, f in enumerate(self.feature_maps):  # feature_maps: (38,19,10,5,3,1)
             for i, j in product(range(f), repeat=2):
                 # 特徴量マッピングの画像サイズ
-                f_k = self.image_size / self.steps[n] # self.steps[n]: 8,16,32,64,
-                
+                f_k = self.image_size / self.steps[n]  # self.steps[n]: 8,16,32,64,
+
                 # dboxの中心座標x,y (但し、0~1で規格化している)
                 # 1 pixel毎にbboxを作成するため、+0.5すると1*1の中心部分に相当する
                 # f_k(特徴量マップのサイズ)で割ることで中心座標を0~1に規格化している
-                cx = (j + 0.5) / f_k # dboxの中心x座標
-                cy = (i + 0.5) / f_k # dboxの中心y座標
+                cx = (j + 0.5) / f_k  # dboxの中心x座標
+                cy = (i + 0.5) / f_k  # dboxの中心y座標
 
-                # アスペクト比1の小さいdbox(cx, cy, width, height)
-                
+                # アスペクト比(1,1)の小さいdbox(cx, cy, width, height)
+                # min_sizes: [30,60,111,162,213,264]
+                s_k = self.min_sizes[n] / self.image_size  # Dboxの1辺に長さ
+                mean.append([cx, cy, s_k, s_k])
+
+                # アスペクト比(1,1)の大きいdbox(cx, cy, width, height)
+                s_k_prime = np.sqrt(s_k * (self.max_sizes[n] / self.image_size))
+                mean.append([cx, cy, s_k_prime, s_k_prime])
+
+                # その他アスペクト比のDboxたち
+                for ar in self.aspect_ratio[n]:  # ex)1,2,3,1/2,1/3
+                    mean.append(
+                        [cx, cy, s_k * np.sqrt(ar), s_k / np.sqrt(ar)]
+                    )  # ボックスの幅および高さを逆にする
+                    mean.append([cx, cy, s_k / np.sqrt(ar), s_k * np.sqrt(ar)])
+
+        output = torch.Tensor(mean).view(-1, 4)  # view：第2引数として設定した値x ⇒ (〇行, x列)に自動変換
+        output = torch.clamp(output, max=1, min=0)  # dboxが画像からはみ出さないようにmin maxでのクリップ
+        return output
 
 
+class SSD(nn.Module):
+    def __init__(self, phase, cfg):
+        super(SSD, self).__init__()
+        self.phase = phase
+        self.num_classes = cfg["num_classes"]
+
+        # SSDのネットワークを作成する
+        self.vgg = generate_vgg()  # vggモジュールを作成
+        self.extra = generate_extra()  # extraモジュールを作成
+        self.L2Norm = L2Norm()  # L2Normクラス
+        self.loc, self.conf = generate_loc_and_conf(
+            num_classes=cfg["num_classes"],
+            bbox_aspect_num=cfg["bbox_aspect_num"],
+        )
+
+        # dboxの作成
+        dbox = DefaultBox(cfg_ssd)
+        self.dbox_list = dbox.generate_dbox_list()
+
+        # 推論時には"Detect"クラスを使用
+        if self.phase == "interface":
+            self.detect = Detect()
 
 
 def make_data_path_list(root_dir):
@@ -286,7 +338,10 @@ def main_run():
     vgg = generate_vgg()
     extra = generate_extra()
     loc, conf = generate_loc_and_conf()
-    print(loc, conf)
+    dbox = DefaultBox(cfg_ssd)
+    dbox_list = dbox.generate_dbox_list()
+    ssd = SSD(phase="train", cfg=cfg_ssd)
+    print(ssd)
 
 
 if __name__ == "__main__":
